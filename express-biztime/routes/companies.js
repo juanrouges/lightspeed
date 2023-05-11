64e8742aecfc24f3a6ea4c6052a7f0860e468ef6
Juan Rojas <juanrojas1912@gmail.com>
2023-05-11 10:53:47 -0400

    response with compnies stored in db

--- express-biztime/routes/companies.js
+++ express-biztime/routes/companies.js
@@ -1,9 +1,10 @@
-const db = require('../db');
-const express = require('express');
+const db = require("../db");
+const express = require("express");
 const router = new express.Router();
 
-router.get('/', async (req, res) => {
-  return res.json({ test: 'Hello World' })
-})
+router.get("/", async (req, res) => {
+  const companiesList = await db.query("SELECT * FROM companies");
+  return res.json({ companies: companiesList.rows });
+});
 
 module.exports = router;
