d6a23fd18fbe5429b9bed4710ff14f445e4cbb5f
Juan Rojas <juanrojas1912@gmail.com>
2023-09-05 17:09:47 -0400

    express jobbly First Task: sqlForPartialUpdate

--- express-jobly/helpers/sql.js
+++ express-jobly/helpers/sql.js
@@ -1,42 +1,41 @@
-const { BadRequestError } = require("../expressError");
+const { BadRequestError } = require('../expressError');
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
 
 /**
+ * params:
  * dataToUpdate = request data to be change
  * jsToSql = sql key map js format to sql format
  **/
-
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
-  console.log("DATA TO UPDATE", dataToUpdate);
-  console.log("JS TO SQL", jsToSql);
+  console.log('DATA TO UPDATE', dataToUpdate);
+  console.log('JS TO SQL', jsToSql);
 
   const keys = Object.keys(dataToUpdate);
-  if (keys.length === 0) throw new BadRequestError("No data");
-
-  console.log("KEYS", keys);
+  // if resquest data is null thro error
+  if (keys.length === 0) throw new BadRequestError('No data');
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map(
-    // Why jsToSql[colName] if not exist why colName
+    // use jsToSql object to set keys, otherwise use request data keys
     (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
   );
 
   console.log(
     keys.map((colName, idx) =>
-      console.log("JS TO SQL COL NAME", jsToSql[colName])
+      console.log('JS TO SQL COL NAME', jsToSql[colName])
     )
   );
-  console.log(keys.map((colName, idx) => console.log("COL NAME", colName)));
+  console.log(keys.map((colName, idx) => console.log('COL NAME', colName)));
 
-  console.log("COLS", cols.join(", "));
+  console.log('COLS', cols.join(', '));
   // COLS "first_name"=$1, "last_name"=$2
 
-  console.log("VALUES", Object.values(dataToUpdate));
+  console.log('VALUES', Object.values(dataToUpdate));
   // VALUES [ 'Colin', 'Wood' ]
 
   return {
-    setCols: cols.join(", "),
+    setCols: cols.join(', '),
     values: Object.values(dataToUpdate),
   };
 }
